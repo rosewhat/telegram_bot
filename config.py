d298ac4ff998f65bfac7ea6e8167dd139c4400d5
rosewhat <whatcodingrose@gmail.com>
2022-03-28 00:10:10 +0300

    2 commit

--- config.py
+++ config.py
@@ -1,1 +1,42 @@
-open_weather = '16a33ab2ab649ae530b7e7ef61ccab5e'
+import json
+
+import requests
+
+
+def top_rated():
+    req = requests.get(
+        "https://api.themoviedb.org/3/movie/top_rated?api_key=d10554b639e8af1d820f7ec858bc7a02&language=ru-US&page=1")
+    response = req.json()
+    s = ""
+    for i in response['results']:
+        s += i['title'] + "\n" + "Оригинальная озвучка  : " + i['original_language'] + "\n" + "Рейтинг: " + str(
+            i['vote_average'])
+        s += "\n"
+        s += "\n"
+    return s
+
+
+def now_playing():
+    req = requests.get(
+        "https://api.themoviedb.org/3/movie/now_playing?api_key=d10554b639e8af1d820f7ec858bc7a02&language=en-US&page=1")
+    response = req.json()
+    s = ""
+    for i in response['results']:
+        s += i['title'] + "\n" + "Оригинальная озвучка  : " + i['original_language'] + "\n" + "Рейтинг: " + str(
+            i['vote_average'])
+        s += "\n"
+        s += "\n"
+    return s
+
+
+def popular_actors():
+    req = requests.get(
+        "https://api.themoviedb.org/3/person/popular?api_key=d10554b639e8af1d820f7ec858bc7a02&language=en-US&page=1")
+    response = req.json()
+    s = ""
+    for i in response['results']:
+        s += i['name'] + "\n" + "Популярность : " + str(i['popularity']) + "\n" + "Гендер: " + str(
+            i['gender'])
+        s += "\n"
+        s += "\n"
+    return s
--- main.py
+++ main.py
@@ -1,29 +1,16 @@
 import telebot
 import requests
-from config import open_weather
+
+import config
+from config import top_rated
+from config import now_playing
+from config import popular_actors
 # обращение к телепорту
 from telebot import types
 
-TOKEN = 'xxx'
+TOKEN = '5203866796:AAGCC8E0YlMO-AllwGzNrlsugXUKjtFX5do'
 bot = telebot.TeleBot(TOKEN)
-import requests
 from pprint import pprint
-from config import open_weather
-import requests
-from pprint import pprint
-
-
-def get_weather():
-    r = requests.get(
-        f"https://api.themoviedb.org/3/movie/top_rated?api_key=d10554b639e8af1d820f7ec858bc7a02"
-    )
-    data = r.json()
-    #  pprint(data)
-    text = ""
-    for status in data["results"]:
-       print(status["title"].encode("utf-8"))
-
-
 
 # обработчик на команду start
 @bot.message_handler(commands=['start'])
@@ -35,7 +22,7 @@ def start(message):
     item_new = types.KeyboardButton('🔥 Новинки кино')
     item_popular = types.KeyboardButton('🎬 Популярное кино')
     item_dock = types.KeyboardButton('🔎 Информация')
-    item_filter = types.KeyboardButton('🙃 Жанры кино')
+    item_filter = types.KeyboardButton('🙃 Актеры')
     markup.add(item_new, item_popular, item_dock, item_filter)
 
     bot.send_message(message.chat.id, "Привет, {0.first_name}!".format(message.from_user), reply_markup=markup)
@@ -47,19 +34,17 @@ def bot_message(message):
     if message.chat.type == 'private':
         if message.text == '🔥 Новинки кино':
             markup = types.ReplyKeyboardMarkup(row_width=2)
-            item1 = types.KeyboardButton('1')
-            item2 = types.KeyboardButton('2')
+
             back = types.KeyboardButton('⬅ Назад')
-            markup.add(item1, item2, back)
-            bot.send_message(message.chat.id, "здесь пока пусто", reply_markup=markup)
+            markup.add(back)
+            bot.send_message(message.chat.id, now_playing(), reply_markup=markup)
+
 
         elif message.text == '🎬 Популярное кино':
             markup = types.ReplyKeyboardMarkup(row_width=2)
-            item1 = types.KeyboardButton('1')
-            item2 = types.KeyboardButton('2')
             back = types.KeyboardButton('⬅ Назад')
-            markup.add(item1, item2, back)
-            bot.send_message(message.chat.id, "здесь пока пусто", reply_markup=markup)
+            markup.add(back)
+            bot.send_message(message.chat.id, top_rated(), reply_markup=markup)
         elif message.text == '🔎 Информация':
             markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
             item_name = types.KeyboardButton('Разработчик')
@@ -73,32 +58,20 @@ def bot_message(message):
             item_new = types.KeyboardButton('🔥 Новинки кино')
             item_popular = types.KeyboardButton('🎬 Популярное кино')
             item_dock = types.KeyboardButton('🔎 Информация')
-            item_filter = types.KeyboardButton('🙃 Жанры кино')
+            item_filter = types.KeyboardButton('🙃 Актеры')
             markup.add(item_new, item_popular, item_dock, item_filter)
             bot.send_message(message.chat.id, "⬅ Назад", reply_markup=markup)
         elif message.text == "Разработчик":
             bot.send_message(message.chat.id, "Антон Московский")
         elif message.text == "О боте":
             markup = types.ReplyKeyboardMarkup(row_width=2)
-            bot.send_message(message.chat.id, f"{get_weather()}", reply_markup=markup)
-        elif message.text == "🙃 Жанры кино":
-            markup_inline = types.InlineKeyboardMarkup(row_width=2)
-            item1 = types.InlineKeyboardButton('Аниме', callback_data='question1')
-            item2 = types.InlineKeyboardButton('Детектив', callback_data='question2')
-            item3 = types.InlineKeyboardButton('Военный', callback_data='question3')
-            item4 = types.InlineKeyboardButton('Комедия', callback_data='question4')
-            item5 = types.InlineKeyboardButton('Мультфильм', callback_data='question5')
-            item6 = types.InlineKeyboardButton('Ужасы', callback_data='question6')
+            bot.send_message(message.chat.id, "Бот берет данные с сайта The Movie DB.", reply_markup=markup)
+        elif message.text == "🙃 Актеры":
+            markup = types.ReplyKeyboardMarkup(row_width=2)
 
-            markup_inline.add(item1, item2, item3, item4, item5, item6)
-            bot.send_message(message.chat.id, "Выбери жанр кино", reply_markup=markup_inline)
-
-
-@bot.callback_query_handler(func=lambda call: True)
-def callback(call):
-    if call.message:
-        if call.data == 'question1':
-            bot.send_message(call.message.chat.id, get_weather())
+            back = types.KeyboardButton('⬅ Назад')
+            markup.add(back)
+            bot.send_message(message.chat.id, popular_actors(), reply_markup=markup)
 
     # не отключался
 
